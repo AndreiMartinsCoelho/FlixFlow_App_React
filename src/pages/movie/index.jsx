9bf42c56579d0774587a7875d6d37c4ddd8f755b
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-09-25 20:07:46 -0300

    Coisas adicionais

--- src/pages/movie/index.jsx
+++ src/pages/movie/index.jsx
@@ -11,7 +11,20 @@ const Movie = () => {
   const imagePath = "https://image.tmdb.org/t/p/w500";
   const [movie, setMovie] = useState({});
   const [genres, setGenres] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [trailerKey, setTrailerKey] = useState("");
   const KEY = process.env.REACT_APP_KEY;
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${KEY}&language=pt-BR&page=1`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setSimilarMovies(data.results);
+      })
+      .catch((err) => console.error(err));
+  }, [KEY, id]);
 
   useEffect(() => {
     fetch(
@@ -21,6 +34,20 @@ const Movie = () => {
       .then((data) => {
         setMovie(data);
         setGenres(data.genres);
+      })
+      .catch((err) => console.error(err));
+  }, [KEY, id]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${KEY}&language=pt-BR`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const trailer = data.results.find((video) => video.type === "Trailer");
+        if (trailer) {
+          setTrailerKey(trailer.key);
+        }
       })
       .catch((err) => console.error(err));
   }, [KEY, id]);
@@ -51,12 +78,15 @@ const Movie = () => {
             alt={movie.title}
           />
           <div className="info">
-            <span className="T">{movie.title}</span>
-            <span className="D">
-              {genres.map((genre) => (
-                <span key={genre.id} className="genNome">{genre.name} </span>
-              ))}
-            </span>
+            {genres.length > 0 && (
+              <span className="D">
+                {genres.map((genre) => (
+                  <span key={genre.id} className="genNome">
+                    {genre.name}{" "}
+                  </span>
+                ))}
+              </span>
+            )}
             <span className="D">Data de lançamento: {movie.release_date}</span>
             <span className="D">Nota: {movie.vote_average}</span>
             <span className="D">Idioma: {movie.original_language}</span>
@@ -66,9 +96,52 @@ const Movie = () => {
             </span>
             <span className="D">Popularidade: {movie.popularity}</span>
             <span className="D">Descrição: {movie.overview}</span>
+            <span className="D">Status: {movie.status}</span>
+            <span className="D">Orçamento: {movie.budget} dólares</span>
+            <span className="D">Receita: {movie.revenue} dólares</span>
+            {trailerKey && (
+              <div className="trailer">
+                <h2>Trailer:</h2>
+                <iframe
+                  className="trailer"
+                  src={`https://www.youtube.com/embed/${trailerKey}`}
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; gyroscope; picture-in-picture"
+                ></iframe>
+              </div>
+            )}
             <Link to="/">
               <button className="link_button">Voltar</button>
             </Link>
+          </div>
+        </div>
+        <div className="related-movies">
+          <div className="movie-grid">
+          <h2>Filmes Relacionados:</h2>
+            {similarMovies.map((movie) => (
+              <Link key={movie.id} to={`/movie/${movie.id}`} className="Filme3">
+                <img
+                  src={`${imagePath}${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <span className="Titulo">{movie.title}</span>
+                <div className="info">
+                  <span className="Data">
+                    Data de lançamento: {movie.release_date}
+                  </span>
+                  <span className="Nota">Nota: {movie.vote_average}</span>
+                  <span className="Idioma">
+                    Idioma: {movie.original_language}
+                  </span>
+                  <span className="Popularidade">
+                    Popularidade: {movie.popularity}
+                  </span>
+                </div>
+                <Link to={`/${movie.id}`} className="">
+                  <button className="btnVer">Saiba mais...</button>
+                </Link>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
